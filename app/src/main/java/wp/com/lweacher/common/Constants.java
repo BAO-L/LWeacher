9ae997e55d1fbc0bc2031a5df9f4fa1742aa5a0a
BAO <1194899285@qq.com>
2017-11-18 09:41:51 +0800

    test

---
+++ app/src/main/java/wp/com/lweacher/common/Constants.java
@@ -0,0 +1,11 @@
+package wp.com.lweacher.common;
+
+/**
+ * Created by wpuser on 2017/11/18.
+ */
+
+public class Constants {
+
+    public static final String BASE_URL = "http://guolin.tech/api/weather?";
+    public static final String KEY = "ee51f77e11144421903399d6dd3baefd";
+}
---
+++ app/src/main/java/wp/com/lweacher/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package wp.com.lweacher.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import wp.com.lweacher.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
